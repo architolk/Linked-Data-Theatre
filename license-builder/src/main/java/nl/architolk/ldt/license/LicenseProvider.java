53182cac55ccbc1787bf0b94d2e7b64245309af1
architolk <msj@xs4all.nl>
2016-01-20 20:21:43 +0100

    Version enhancement for header checking

--- license-builder/src/main/java/nl/architolk/ldt/license/LicenseProvider.java
+++ license-builder/src/main/java/nl/architolk/ldt/license/LicenseProvider.java
@@ -13,6 +13,7 @@ import java.text.SimpleDateFormat;
 public class LicenseProvider implements PropertiesProvider {
 
 	public static final String FILE_TIMESTAMP = "file.date";
+	public static final String FILE_VERSION = "file.version";
 
 	public LicenseProvider() {
 		super();
@@ -21,9 +22,21 @@ public class LicenseProvider implements PropertiesProvider {
 	public Map<String, String> getAdditionalProperties(AbstractLicenseMojo mojo, Properties properties, Document document) {
 		try {
 			Map<String, String> result = new HashMap<String, String>();
+			//Set date to current date of file
 			SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
 			Date timestamp = new Date(document.getFile().lastModified());
-			result.put(FILE_TIMESTAMP, dt.format(timestamp));
+			String fileDate = dt.format(timestamp);
+			result.put(FILE_TIMESTAMP, fileDate);
+			//Set file version to project version OR release version if document date is release date
+			String projectVersion = properties.getProperty("project.version");
+			String releaseVersion = mojo.properties.get("release.version");
+			String releaseDate = properties.getProperty("release.date");
+			if (fileDate.equals(releaseDate)) {
+				result.put(FILE_VERSION,releaseVersion);
+			}
+			else {
+				result.put(FILE_VERSION,projectVersion);
+			}
             return Collections.unmodifiableMap(result);
 		} catch (Exception e) {
 			throw new RuntimeException("Error excuting LDT license provider", e);
