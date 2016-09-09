da9b9961d5838e1155249d6faf6efb4d8c1d5bbe
architolk <msj@xs4all.nl>
2016-09-09 21:49:01 +0200

    Added test cases

--- examples/README.md
+++ examples/README.md
@@ -1,2 +1,5 @@
 #Examples
 This folder contains a list of examples for the Linked Data Theatre. It contains all the features that are available in the Linked Data Theatre.
+
+##Regression test
+This folder also contains a couple of tests to check if all functionality of the LDT is working correctly.
--- examples/deploy-tests.bat
+++ examples/deploy-tests.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Empty graph
 curl.exe -X PUT -T empty.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/stage
-cd tutorial
+cd tests
 for %%y in (*.ttl) do (
 	echo %%~ny
 	curl.exe -X POST -T %%y http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/stage
