1247186a81825f8516b80167872504c8365d2355
Lars Wortel <lars.wortel@capgemini.com>
2017-04-21 08:18:43 +0200

    Minor change in batch files to dramatically increase performance

--- examples/deploy-examples.bat
+++ examples/deploy-examples.bat
@@ -1,9 +1,16 @@
 @echo off
-echo Empty graph
+echo Clearing graph...
 curl.exe -X PUT -T empty.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/stage
+type NUL > concat.ttl
 cd tutorial
 for %%y in (*.ttl) do (
-	echo %%~ny
-	curl.exe -X POST -T %%y http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/stage
+	echo Processing %%~ny...
+	type %%y >> "../concat.ttl"
+	echo. >> "../concat.ttl"
 )
+cd ..
+echo Deploying...
+curl.exe -X POST -T concat.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/stage
+del concat.ttl
+echo Done.
 pause
