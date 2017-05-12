63a579f3c6bb389e6dcfcc3ea041b0ecc37fcd28
architolk <msj@xs4all.nl>
2017-05-12 15:54:26 +0200

    Restructure menu for tests

--- examples/deploy-basic-configuration.bat
+++ examples/deploy-basic-configuration.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Deploying basic configuration...
-curl.exe -X PUT -T ../basic-configuration.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://localhost:8080/stage
+curl.exe -X PUT -T ../basic-configuration.ttl http://localhost:8080/backstage/import
 echo Done.
 pause
