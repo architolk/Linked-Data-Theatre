89a1ebaad63af68426c9ce09c0ca92f6ae5a11f9
architolk <msj@xs4all.nl>
2017-05-15 16:34:57 +0200

    Updated rdf4j processor to comply with sparql-1.1

--- processors/src/main/java/nl/architolk/ldt/processors/RDF4JProcessor.java
+++ processors/src/main/java/nl/architolk/ldt/processors/RDF4JProcessor.java
@@ -50,6 +50,12 @@
  * - replace: remove all previous content and insert triple into container and into target graph
  * - update: remove all properties from subjects of target graph that are present in new container and insert new triples into target graph
  * - part: remove old triples from target graph, clear container, insert triples into container and new triples from container into target graph
+ *
+ * NITTY-GRITTY-THINGY:
+ * All rdf4j queries seem to use a default graph. A "where" clause without a "from" or "graph ?g" statement (but for example a "graph <..>")
+ * will NOT return only statement from the default graph!!! This is not compatible with a sparql statement without a default graph
+ * some postquery statements might not work as expected
+ *
  */
 package nl.architolk.ldt.processors;
 
@@ -69,6 +75,7 @@ import org.apache.log4j.Logger;
 import org.orbeon.oxf.util.LoggerFactory;
 
 import virtuoso.rdf4j.driver.VirtuosoRepository;
+import org.eclipse.rdf4j.IsolationLevels;
 import org.eclipse.rdf4j.repository.http.HTTPRepository;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
@@ -104,142 +111,164 @@ public class RDF4JProcessor extends SimpleProcessor {
 		String pgraph = configNode.valueOf("pgraph"); // The parent graph, for version information
 		String postQuery = configNode.valueOf("postquery"); // Some post query, optional
 		
+		String errorMsg = "";
+		
 		db = RDF4JProperties.createRepository();
 		if (db==null) {
-			String msg = "Unknown database: ";
-			contentHandler.characters(msg.toCharArray(),0,msg.length());
+			errorMsg = "Unknown database. \n";
 		} else {
 			conn = db.getConnection();
 
-			// Clear target graph, partially (all triples in original container) or completely
-			if (action.equals("replace")) {
-				String msg = "Target graph cleared";
-				try {
-					IRI tgraphResource = db.getValueFactory().createIRI(tgraph);
-					conn.clear(tgraphResource);
+			try {
+				conn.begin(IsolationLevels.NONE);
+				
+				// Clear target graph, partially (all triples in original container) or completely
+				if (action.equals("replace")) {
+					String msg = "Target graph <" + tgraph + "> cleared";
+					try {
+						IRI tgraphResource = db.getValueFactory().createIRI(tgraph);
+						conn.clear(tgraphResource);
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
+				} else if (action.equals("part")) {
+					String msg ="Target graph <" + tgraph + "> partially cleared";
+					try {
+						conn.prepareUpdate("delete { graph <" + tgraph + "> {?s?p?o}} using <" + cgraph + "> where {?s?p?o}").execute();
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
-				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			} else if (action.equals("part")) {
-				String msg ="Target graph partially cleared";
-				try {
-					conn.prepareUpdate("delete from <" + tgraph + "> {?s?p?o} where { graph <" + cgraph + "> {?s?p?o}}").execute();
-				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
-				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			}
 
-			// Clear container, except when action = insert
-			if (!action.equals("insert")) {
-				String msg = "Container cleared";
-				try {
-					IRI cgraphResource = db.getValueFactory().createIRI(cgraph);
-					conn.clear(cgraphResource);
+				// Clear container, except when action = insert
+				if (!action.equals("insert")) {
+					String msg = "Container <" + cgraph + "> cleared";
+					try {
+						IRI cgraphResource = db.getValueFactory().createIRI(cgraph);
+						conn.clear(cgraphResource);
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
-				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			}
 
-			// Insert documents into container graph
-			Document dataDocument = readInputAsDOM4J(context, INPUT_DATA);
-			List filelist = dataDocument.selectNodes("//filelist//file");
-			Iterator<?> elit = filelist.listIterator();
-			while (elit.hasNext()) {
-				Node child = (Node) elit.next();
-				String msg = "file uploaded";
-				try {
-					uploadFile(child.valueOf("@name"),child.getText(),cgraph);
+				// Insert documents into container graph
+				Document dataDocument = readInputAsDOM4J(context, INPUT_DATA);
+				List filelist = dataDocument.selectNodes("//filelist//file");
+				Iterator<?> elit = filelist.listIterator();
+				while (elit.hasNext()) {
+					Node child = (Node) elit.next();
+					String msg = "file uploaded";
+					try {
+						uploadFile(child.valueOf("@name"),child.getText(),cgraph);
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
+				
+				// Remove existing properties in case of action = update
+				if (action.equals("update")) {
+					String msg ="Target graph cleared for update";
+					try {
+						conn.prepareUpdate("delete {graph <" + tgraph + "> {?s?x?y}} using <" + cgraph + "> using <" + tgraph + "> where {graph <" + tgraph + "> {?s?x?y} graph <" + cgraph + "> {?s?p?o}}").execute();
+						// Remove orphant blank nodes (to third degree, beter option could be to count the number of deleted nodes and repeat when not equal to zero)
+						conn.prepareUpdate("delete {graph <" + tgraph + "> {?bs?bp?bo}} using <" + tgraph + "> where {?bs?bp?bo FILTER(isblank(?bs)) FILTER NOT EXISTS {?s?p?bs}}").execute();
+						conn.prepareUpdate("delete {graph <" + tgraph + "> {?bs?bp?bo}} using <" + tgraph + "> where {?bs?bp?bo FILTER(isblank(?bs)) FILTER NOT EXISTS {?s?p?bs}}").execute();
+						conn.prepareUpdate("delete {graph <" + tgraph + "> {?bs?bp?bo}} using <" + tgraph + "> where {?bs?bp?bo FILTER(isblank(?bs)) FILTER NOT EXISTS {?s?p?bs}}").execute();
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			}
-			
-			// Remove existing properties in case of action = update
-			if (action.equals("update")) {
-				String msg ="Target graph cleared for update";
-				try {
-					conn.prepareUpdate("delete from <" + tgraph + "> {?s?x?y} where { graph <" + cgraph + "> {?s?p?o}}").execute();
-					// Remove orphant blank nodes (to third degree, beter option could be to count the number of deleted nodes and repeat when not equal to zero)
-					conn.prepareUpdate("delete from <" + tgraph + "> {?bs?bp?bo} where { graph <" + tgraph + "> {?bs?bp?bo FILTER(isblank(?bs)) FILTER NOT EXISTS {?s?p?bs}}}").execute();
-					conn.prepareUpdate("delete from <" + tgraph + "> {?bs?bp?bo} where { graph <" + tgraph + "> {?bs?bp?bo FILTER(isblank(?bs)) FILTER NOT EXISTS {?s?p?bs}}}").execute();
-					conn.prepareUpdate("delete from <" + tgraph + "> {?bs?bp?bo} where { graph <" + tgraph + "> {?bs?bp?bo FILTER(isblank(?bs)) FILTER NOT EXISTS {?s?p?bs}}}").execute();
+				
+				// Populate target graph with content of the container-graph
+				if (action.equals("part") || action.equals("replace") || action.equals("update")) {
+					String msg ="Target graph <" + tgraph + "> populated from container <" + cgraph + ">";
+					try {
+						conn.prepareUpdate("insert { graph <" + tgraph + "> {?s?p?o}} using <" + cgraph + "> where {?s?p?o}").execute();
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
+				
+				// Insert version-info into parent graph, if applicable
+				if (!(cgraph.equals(pgraph) || pgraph.isEmpty())) {
+					String msg ="Version metadata inserted into parent graph";
+					try {
+						conn.prepareUpdate("insert data {graph <" + pgraph + "> {<" + pgraph + "> <http://purl.org/dc/terms/hasVersion> <" + cgraph + ">}}").execute();
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			}
-			
-			// Populate target graph with content of the container-graph
-			if (action.equals("part") || action.equals("replace") || action.equals("update")) {
-				String msg ="Target graph populated";
-				try {
-					conn.prepareUpdate("insert into <" + tgraph + "> {?s?p?o} where { graph <" + cgraph + "> {?s?p?o}}").execute();
+				
+				// Execute post query
+				if (!postQuery.isEmpty()) {
+					String msg ="Post query executed";
+					try {
+						conn.prepareUpdate(postQuery).execute();
+					}
+					catch (Exception e) {
+						// In case of an error, put the errormessage in the result, but don't throw the exception
+						msg = e.toString();
+						errorMsg += e.getMessage() + ". \n";
+					}
+					contentHandler.startElement("", "scene", "scene", new AttributesImpl());
+					contentHandler.characters(msg.toCharArray(),0,msg.length());
+					contentHandler.endElement("", "scene", "scene");
 				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
-				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			}
-			
-			// Insert version-info into parent graph, if applicable
-			if (!(cgraph.equals(pgraph) || pgraph.isEmpty())) {
-				String msg ="Version metadata inserted into parent graph";
-				try {
-					conn.prepareUpdate("insert into <" + pgraph + "> {<" + pgraph + "> <http://purl.org/dc/terms/hasVersion> <" + cgraph + ">}").execute();
-				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
-				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
-			}
-			
-			// Execute post query
-			if (!postQuery.isEmpty()) {
-				String msg ="Post query executed";
-				try {
-					conn.prepareUpdate(postQuery).execute();
-				}
-				catch (Exception e) {
-					// In case of an error, put the errormessage in the result, but don't throw the exception
-					msg = e.getMessage();
-				}
-				contentHandler.startElement("", "scene", "scene", new AttributesImpl());
-				contentHandler.characters(msg.toCharArray(),0,msg.length());
-				contentHandler.endElement("", "scene", "scene");
+				conn.commit();
+				
+			} finally {
+				conn.close();
 			}
 			
 		}
 		
+		if (!errorMsg.isEmpty()) {
+			contentHandler.startElement("", "error", "error", new AttributesImpl());
+			contentHandler.characters(errorMsg.toCharArray(),0,errorMsg.length());
+			contentHandler.endElement("", "error", "error");
+		}
 		contentHandler.endElement("", "response", "response");
 		contentHandler.endDocument();
 	}
