ff6c5608a7be3d342d5ffa786210968498522d6d
architolk <msj@xs4all.nl>
2017-06-09 22:08:26 +0200

    Updated vocabulary description and added first version of 2.0 vocabulary

--- vocabulary/deploy-vocabulary.bat
+++ vocabulary/deploy-vocabulary.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Empty graph and populate with elmo vocabulary definition
 curl.exe -X PUT -T elmo.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://bp4mc2.org/elmo/def
+curl.exe -X PUT -T elmo2.ttl http://localhost:8890/sparql-graph-crud?graph-uri=http://bp4mc2.org/elmo2/def
 pause
