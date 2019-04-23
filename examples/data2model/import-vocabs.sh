87c5c0505fff9736894f1112f06cf5607232a4f3
architolk <msj@xs4all.nl>
2019-04-23 16:32:20 +0200

    Fixed import

--- examples/data2model/import-vocabs.sh
+++ examples/data2model/import-vocabs.sh
@@ -2,7 +2,18 @@
 for vocab in $(cat vocabs.txt)
 do
   echo $vocab
-  curl -X GET -L -H "Accept: application/rdf+xml" $vocab > vocab.xml
-  curl -X PUT -H "Content-Type: application/rdf+xml" -T vocab.xml http://localhost:8080/data2model/container/import
+#  curl -s -X GET -L -H "Accept: text/turtle,application/rdf+xml" "$vocab" > vocab
+  curl -s -X GET -L -H "Accept: text/turtle,application/rdf+xml" "$vocab" > vocab
+  if file vocab | grep -qi "XML\|SGML"
+  then
+    echo "(XML)"
+    mv vocab vocab.xml
+    curl -X PUT -H "Content-Type: application/rdf+xml" -T vocab.xml http://localhost:8080/data2model/container/import
+    rm -f vocab.xml
+  else
+    echo "(Turtle)"
+    mv vocab vocab.ttl
+    curl -X PUT -H "Accept: text/plain" -H "Content-Type: text/turtle" -T vocab.ttl http://localhost:8080/data2model/container/import
+    rm -f vocab.ttl
+  fi
 done
-rm -f vocab.xml
