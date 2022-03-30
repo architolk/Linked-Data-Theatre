09c4badf000a1f14cf2386450d29b091135283a5
architolk <msj@xs4all.nl>
2022-03-30 19:52:06 +0200

    update examples

--- examples/data2model/deploy.sh
+++ examples/data2model/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 rm -f config.zip
 zip config.zip *.ttl
+cd extensions
+zip ../config.zip *.ttl
+cd ..
 curl -X PUT -H "Content-Type: multipart/x-zip" -T config.zip http://localhost:8080/data2model/backstage/import
